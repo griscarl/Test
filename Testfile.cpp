5aceb4f5da5d664c20c33168f41ddf073a30c754
griscarl <gris4carl@hotmail.com>
2017-03-08 10:52:33 +0800

    Example Code Roger

--- Testfile.cpp
+++ Testfile.cpp
@@ -1,2 +1,11 @@
-Hello balle
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int x, y;
+	cout << "Please enter 2 numbers: ";
+	cin >> x >> y;
+	cout << x << "+" << y << "=" << x+y << endl;
+	cin.get();
+	return 0;
+}
